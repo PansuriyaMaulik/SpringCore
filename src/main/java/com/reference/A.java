935279b4d8e45059fc15b122b8429d60c6b9cc42
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-11-24 10:14:35 +0100

    Class A inject objcet of class A

---
+++ src/main/java/com/reference/A.java
@@ -0,0 +1,33 @@
+package com.reference;
+
+public class A {
+    private int x;
+    private B obj;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public B getObj() {
+        return obj;
+    }
+
+    public void setObj(B obj) {
+        this.obj = obj;
+    }
+
+    public A() {
+    }
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "x=" + x +
+                ", obj=" + obj +
+                '}';
+    }
+}
