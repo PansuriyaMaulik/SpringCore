166c47a7fa1ff3240f6357578cd1ae5d86bb5b0e
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-11-24 10:48:43 +0100

    Inject constructor using object reference

---
+++ src/main/java/com/constructor/Constructor.java
@@ -0,0 +1,13 @@
+package com.constructor;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Constructor {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("constructorconfig.xml");
+
+       Person person = (Person) context.getBean("person");
+        System.out.println(person);
+    }
+}
